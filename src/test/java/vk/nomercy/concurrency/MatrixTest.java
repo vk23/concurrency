c07a18d107983ce5438d390680cf5828f161cf7a
vk <kaligularnd@gmail.com>
2016-10-29 21:32:42 +0300

    test multiplication improved

--- src/test/java/vk/nomercy/concurrency/MatrixTest.java
+++ src/test/java/vk/nomercy/concurrency/MatrixTest.java
@@ -30,19 +30,19 @@ public class MatrixTest {
 
 	@Test
 	public void testMatrixMultiplication() {
-		int[][] m1 = new int[][] { { 1, 2 }, { 1, 2 } };
-		int[][] m2 = new int[][] { { 1 }, { 1 } };
-		int[][] expected = new int[][] { { 3 }, { 3 } };
+		int[][] m1 = { { 1, 2 }, { 4, 5 }, { 7, 8 } };
+		int[][] m2 = { { 1, 4, 7 }, { 2, 5, 8 } };
+		int[][] expected = { { 5, 14, 23 }, { 14, 41, 68 }, { 23, 68, 113 } };
 
 		Matrix matrix1 = new Matrix(m1);
 		Matrix matrix2 = new Matrix(m2);
 
 		Matrix res = matrix1.multiply(matrix2);
 		MatrixUtil.print("Multiply result:", res.getArray());
-		assertArrayEquals("Multiply Expected result={{3},{3}}", expected, res.getArray());
+		assertArrayEquals("Multiply result", expected, res.getArray());
 
 		Matrix res2 = matrix1.multiplyConcurrent(matrix2);
 		MatrixUtil.print("Multiply2 (concurrent) result:", res2.getArray());
-		assertArrayEquals("Multiply2 (concurrent) Expected result={{3},{3}}", expected, res2.getArray());
+		assertArrayEquals("Multiply2 (concurrent) result", expected, res2.getArray());
 	}
 }
